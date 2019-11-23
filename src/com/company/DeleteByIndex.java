4dc721a0126d8041038cc50c31efe9fb5e0e4696
dcpiturlea <dcpiturlea@gmail.com>
2019-11-24 00:32:43 +0200

    Create loop for multiple ADD/DEL

--- src/com/company/DeleteByIndex.java
+++ src/com/company/DeleteByIndex.java
@@ -3,14 +3,27 @@ package com.company;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class DeleteByIndex extends AddValueInArray{
+public class DeleteByIndex extends AddValueInArray {
 
     public void DeteleByIndexMethod() {
         Scanner in = new Scanner(System.in);
-        System.out.println("What index want to delete? ");
-        int index = in.nextInt();
-        ArrayStructures.theArray.remove(index);
-        System.out.println("Now");
+        boolean newDel = true;
+        while (newDel) {
+            System.out.println("What index want to delete? ");
+            int index = in.nextInt();
+            ArrayStructures.theArray.remove(index);
+
+            System.out.println("Do you want delete a new value? Y/N :");
+
+            String anotherAdd = in.next();
+
+            if (anotherAdd.toUpperCase().equals("Y")) {
+                newDel = true;
+            } else {
+                newDel = false;
+            }
+
+        }
         ArrayStructures.printArray();
     }
 
